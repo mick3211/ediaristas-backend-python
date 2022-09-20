b96ba6c7ea800b36c3726893e2e84f3c583ff7e7
Mickael <mickaelfelizardo2008@gmail.com>
2022-09-20 10:40:46 -0300

    ;

--- ediaristas/settings.py
+++ ediaristas/settings.py
@@ -16,7 +16,7 @@ import environ
 
 env = environ.Env()
 env.read_env()
-print('-------', env)
+print('-------', env('SECRET_KEY'))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
