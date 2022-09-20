4dbcb6321f7b02114ddcd103a5390f702ab60a67
Mickael <mickaelfelizardo2008@gmail.com>
2022-09-20 16:50:49 -0300

    add dj-database-url

--- ediaristas/settings.py
+++ ediaristas/settings.py
@@ -14,6 +14,7 @@ from datetime import timedelta
 import os
 from pathlib import Path
 import environ
+import dj_database_url
 
 env = environ.Env()
 env.read_env()
@@ -138,16 +139,7 @@ WSGI_APPLICATION = 'ediaristas.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': env('DATABASE_NAME'),
-        'HOST': env('DATABASE_HOST'),
-        'PORT': 3306,
-        'USER': env('DATABASE_USER'),
-        'PASSWORD': env('DATABASE_PASSWORD')
-    }
-}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 #Email
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
